6509e8d97c11e67028ed386fa03b638a3fec3eb4
Pranav Varkey <ptv1324ptv@outlook.com>
2025-06-10 16:33:28 +0530

    Removed all the ROMs and Latches

--- src/RISCV/RISCV.srcs/sources_1/imports/FINAL_PROJECT_RISCV/main_decoder.sv
+++ src/RISCV/RISCV.srcs/sources_1/imports/FINAL_PROJECT_RISCV/main_decoder.sv
@@ -15,27 +15,18 @@ module main_decoder (
 
 logic [16:0] controls;
 
+logic [2:0] ld;
+
+
+
 always @(*) begin
+
+ld = (op[5] == 1'b0) ? funct3 : 3'b0;
+
     case (op)
         // RegWrite_ImmSrc_ALUSrc_MemWrite_ResultSrc_Branch_ALUOp_Jump_Store_Load_Jalr
-        7'b0000011: begin
-                        case (funct3)
-                        3'b000: controls = 17'b1_00_1_0_01_0_00_0_00_000_0; // lb
-                        3'b001: controls = 17'b1_00_1_0_01_0_00_0_00_001_0; // lh
-                        3'b010: controls = 17'b1_00_1_0_01_0_00_0_00_010_0; // lw
-                        3'b100: controls = 17'b1_00_1_0_01_0_00_0_00_011_0; // lbu
-                        3'b101: controls = 17'b1_00_1_0_01_0_00_0_00_100_0; // lhu
-								default:    controls = 17'bx_xx_x_x_xx_x_xx_x_xx_xxx_x; // ???
-                        endcase
-                    end
-        7'b0100011: begin
-                        case (funct3)
-                        3'b000: controls = 17'b0_01_1_1_00_0_00_0_00_000_0; // sw
-                        3'b001: controls = 17'b0_01_1_1_00_0_00_0_01_000_0; // sh
-                        3'b010: controls = 17'b0_01_1_1_00_0_00_0_10_000_0; // sb
-								default:    controls = 17'bx_xx_x_x_xx_x_xx_x_xx_xxx_x; // ???
-                        endcase
-                    end
+        7'b0000011: controls = {13'b1_00_1_0_01_0_00_0_00,funct3,1'b0};
+        7'b0100011: controls = {11'b0_01_1_1_00_0_00_0,funct3[1:0],4'b000_0};
         7'b0110011: controls = 17'b1_xx_0_0_00_0_10_0_00_010_0; // R–type
         7'b1100011: controls = 17'b0_10_0_0_00_1_01_0_00_010_0; // B-type
         7'b0010011: controls = 17'b1_00_1_0_00_0_10_0_00_010_0; // I–type ALU
